d65c38a6bab7939335c8a4b083b6d272048156af
Erik Garcia <erikpumas999@gmail.com>
2024-11-01 20:34:19 -0600

    Se muestra fecha de creacion de la informacion

--- app/src/main/java/com/example/apirestdisney/view/adapters/CharacterViewHolder.kt
+++ app/src/main/java/com/example/apirestdisney/view/adapters/CharacterViewHolder.kt
@@ -7,6 +7,8 @@ import com.example.apirestdisney.data.remote.model.Data
 import com.example.apirestdisney.databinding.CharacterElementBinding
 import com.example.apirestdisney.databinding.FragmentCharacterListBinding
 import com.example.apirestdisney.view.CharacterListFragment
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CharacterViewHolder(
     private val binding: CharacterElementBinding
@@ -20,6 +22,14 @@ class CharacterViewHolder(
             //TODO: poner .error(R.drawable.background_character) 1:34:30 y tambien placeholder
             .into(binding.ivThumbnail)
 
+        binding.tvReleased.text = formatDate(character.createdAt)
     }
 
+}
+
+fun formatDate(dateString: String): String {
+    val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+    val outputFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale.getDefault())
+    val date = inputFormat.parse(dateString)  // String a Date
+    return date?.let { outputFormat.format(it) } ?: dateString  // Formatea
 }
